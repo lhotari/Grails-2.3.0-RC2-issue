5050cbfc6d4ba3633b4f6257276c550aeffb3afc
Lari Hotari <lari.hotari@sagire.fi>
2013-10-23 15:02:13 +0300

    Upgrade to Grails 2.3.2.BUILD-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -53,14 +53,14 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.41"
+        build ":tomcat:7.0.42"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.0.RC2"
+        compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.M6" // or ":hibernate4:4.1.11.M3"
+        runtime ":hibernate:3.6.10.2" // or ":hibernate4:4.1.11.M3"
         runtime ":database-migration:1.3.5"
         runtime ":jquery:1.10.2"
         runtime ":resources:1.2"
